21dbd6ddccbc646b1ef7bf540a6fe60909a0c57f
Jia Chen <36725840+jiachenyee@users.noreply.github.com>
2019-01-15 00:22:44 +0800

    Update Search.swift

--- Search.swift
+++ Search.swift
@@ -26,7 +26,7 @@ extension String {
         case url
     }
     
-    func search(searchEngine: searchEngines) -> UIViewController {
+    func searchVC(searchEngine: searchEngines) -> UIViewController {
         var spliterVariable = "+"
         var urlStart = ""
         var urlEnd = ""
@@ -54,6 +54,36 @@ extension String {
         
         let safariViewController = SFSafariViewController(url: url!)
         return safariViewController
+        
+    }
+    
+    func searchURL(searchEngine: searchEngines) -> URL {
+        var spliterVariable = "+"
+        var urlStart = ""
+        var urlEnd = ""
+        
+        switch searchEngine {
+        case .google:
+            spliterVariable = "%20"
+            urlStart = "google.com/search?hl=en&q="
+        case .duckduckgo:
+            spliterVariable = "+"
+            urlStart = "duckduckgo.com/?q="
+        case .bing:
+            spliterVariable = "+"
+            urlStart = "bing.com/search?q="
+        case .yahoo:
+            spliterVariable = "+"
+            urlStart = "search.yahoo.com/search;_ylt=Awr4xJwErzxcTtEAGExDDWVH;_ylc=X1MDMTE5NzgwNDg2NwRfcgMyBGZyAwRncHJpZANPdV9zck9lSFFxLnlTeEloTlIwOHZBBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4Dc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAwRxc3RybAMxMQRxdWVyeQN0ZXN0JTIwdGVzdAR0X3N0bXADMTU0NzQ4MDg0NA--?fr2=sb-top-search&p="
+            urlEnd = "&fr=sfp&iscqry="
+        }
+        let convertedQuery = self.replacingOccurrences(of: " ", with: spliterVariable)
+        
+        let urlString = "https://\(urlStart)\(convertedQuery)\(urlEnd)"
+        
+        let url = URL(string: urlString)
+        
+        return url
         
     }
 }
